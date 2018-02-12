169a4400d71bf6d1afc610c644fe4e2bc1e270ae
Unknown <pagejakob@gmail.com>
2018-02-12 23:47:57 +1100

    Complete Post page
    Completed newpost page and got to display new posts within the index. Also Implimented the start of displaying individual posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    return (
+      <div className={className}>
+        <label className="h3">{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">{touched ? error : ''}</div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
-    return <div>PostsNew!</div>;
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field label="Catagories" name="catagories" component={this.renderField} />
+        <Field label="Post Content" name="content" component={this.renderField} />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <Link to="/index.js" className="btn btn-danger">
+          Cancel
+        </Link>
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title! (Minimum 3 Charaters)';
+  }
+  if (!values.catagories) {
+    errors.catagories = 'Enter a Catagory!';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content';
+  }
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostNewForm',
+})(connect(null, { createPost })(PostsNew));
